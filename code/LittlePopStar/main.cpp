6873161e840f93cd1626c11deadc9f5abf0702bb
KanuaK <yihan_z@qq.com>
2020-07-20 12:20:25 +0800

    update file structure

--- code/LittlePopStar/main.cpp
+++ code/LittlePopStar/main.cpp
@@ -1,45 +1,56 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <Fl/fl_draw.H>
 #include ".\Common\Stars.h"
+#include ".\Model\Model.h"
 #include <functional>
-Starmat smat;
-class Model {
+#include <memory>
+class TestBox : public Fl_Box {
 public:
-    std::function<bool(int)> sendNotification;
-    void changeData(){
-        //do something
-        sendNotification(3);
-    }
+    TestBox(int x, int y, int w, int h) :Fl_Box(x, y, w, h) {}
+    std::shared_ptr<Starmat> smat;
+private:
+    int handle(int);
+    void draw();
 };
-class ViewModel {
-public:
-    int val;
-    ViewModel():val(0) {}
-    bool receiveNotification(int _val) {
-        this->val = _val;
-        return true;
+int TestBox::handle(int event) {
+    if (event == FL_PUSH && Fl::event_button() == 1) {
+        int x = Fl::event_x(), y = Fl::event_y();
+        int bx = this->x(), by = this->y();
+        printf("%d %d\n", x-bx, y-by);
+        int w = 100, h = 100;
+        int _c = (x - bx) / w;
+        int _r = (by + this->h() - y) / h;
+        printf("%d %d\n", _r, _c);
+        return 1;
     }
-};
+    return 0;
+}
+void TestBox::draw() {
+    static Fl_Color colortable[] = { FL_BLACK,FL_RED,FL_GREEN,FL_YELLOW,FL_BLUE };
+    int n = smat->size();
+    int m = smat->begin()->size();
+    int w = 100, h = 100;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j) {
+            Star s = smat->getStar(i,j);
+            int _color = s.getColor(), _pickup = s.getPickup();
+            if (_color == -1) return;
+            int posx = this->x() + i * w;
+            int posy = this->y() + this->h() - h - j * h;
+            fl_rect(posx, posy, w-5, h-5, colortable[_color]);
+        }
+    return;
+}
+StarMap stm(5, 5, 5);
 int main(int argc, char* argv[]) {
     //some test
-    //smat = Starmat(5, 5);
-    Model _m;
-    ViewModel _vm;
-    _m.sendNotification = std::bind(&ViewModel::receiveNotification, &_vm, std::placeholders::_1);
-    printf("%d\n", _vm.val);
-    _m.changeData();
-    printf("%d\n", _vm.val);
-    
-    //Hello World
     Fl_Window* window;
-    Fl_Box* box;
-    window = new  Fl_Window(450, 330, "First App!");
-    box = new Fl_Box(20, 40, 360, 200, "Hello World");
-    box->box(FL_FLAT_BOX);
-    box->labelsize(36);
-    box->labelfont(FL_BOLD + FL_ITALIC);
-    box->labeltype(FL_SHADOW_LABEL);
+    TestBox* box;
+    window = new  Fl_Window(1080, 768, "LittlePopStar");
+    box = new TestBox(40, 34, 1000, 700);
+    box->smat = stm.getStarmat();
     window->end();
     window->show(argc, argv);
     return  Fl::run();
