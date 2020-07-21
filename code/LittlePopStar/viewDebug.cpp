02f3a9df8d6f03f53c827038ae1145f0c53569c4
joshuamalmberg <josh.m.malmberg@gmail.com>
2020-07-21 14:08:47 -0400

    Removed main function overload.

--- code/LittlePopStar/viewDebug.cpp
+++ code/LittlePopStar/viewDebug.cpp
@@ -17,14 +17,14 @@ void test(int x, int y) {
 	return;
 }
 
-int main(int argc, char* argv[]) {
-	MainWindow* win;
-	StarMap* model = new StarMap(5, 5, 5);
-	win = new MainWindow(5, 5);
-	win->get_StarMapView().attachModel(std::shared_ptr<StarMap>(model));
-	win->get_StarMapView().update(0);
-	win->get_StarMapView().attach_PickupCommand(std::function<void(int, int)>(test));
-	std::cout << "hi";
-	win->show();
-	return  Fl::run();
-}
+//int main(int argc, char* argv[]) {
+//	MainWindow* win;
+//	StarMap* model = new StarMap(5, 5, 5);
+//	win = new MainWindow(5, 5);
+//	win->get_StarMapView().attachModel(std::shared_ptr<StarMap>(model));
+//	win->get_StarMapView().update(0);
+//	win->get_StarMapView().attach_PickupCommand(std::function<void(int, int)>(test));
+//	std::cout << "hi";
+//	win->show();
+//	return  Fl::run();
+//}
